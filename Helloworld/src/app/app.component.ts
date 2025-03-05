3b142898106dc2bd016fc7d73b686b8d4be06c02
GaganTantwar <panthigagan478@gmail.com>
2025-03-05 22:33:47 +0530

    Gagan Tantwar Extended feat : Ability to display Hello from BridgeLabz by user inputted name

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -12,10 +12,11 @@ export class AppComponent {
   title = 'Helloworld';
   imageUrl="../assets/BL_logo_square_jpg.jpg";
   url="https://www.bridgelabz.com";
+  userName: string="";
   ngOnInit():void{
     this.title="Hello From BridgeLab"
   }
-  onClick($event: any){
+  onClick($event:any){
     console.log("Save Button is clicked",$event);
     window.open(this.url,"_blank");
   }
